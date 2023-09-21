2adefe93a85aaa8d8748a2b4b87dda3b54215a69
LakatosJ <128583426+LakatosJ@users.noreply.github.com>
2023-09-21 10:36:24 +0200

    Add files via upload

---
+++ 3-4 óra/feladat_005.py
@@ -0,0 +1,6 @@
+felhasználó_kora = 16
+print(f"A felhasználó_kora változó típusa: {type(felhasználó_kora)}")
+felhasználó_kora = str(felhasználó_kora)
+print(f"A felhasználó_kora változó típusa: {type(felhasználó_kora)}")
+ilyen = input('És milyen ' + felhasználó_kora + ' évesnek lenni? ')
+olyan = input(f'És milyen {felhasználó_kora} évesnek lenni? ')
---
+++ 3-4 óra/feladat_006.py
@@ -0,0 +1,10 @@
+
+gondolt_szám = 4
+tipp = input('Gondoltam egy számra. Tippeld meg! ')
+tipp = int(tipp)
+if tipp == gondolt_szám:
+    print(f'Ügyes!')
+print('Pápá.')
+
+
+
