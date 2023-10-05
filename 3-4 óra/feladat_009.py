29c9167fd664d1aa3a45fd75a2eaea3ae168cc41
LakatosJ <128583426+LakatosJ@users.noreply.github.com>
2023-10-05 10:30:41 +0200

    gg

--- 3-4 óra/feladat_009.py
+++ 3-4 óra/feladat_009.py
@@ -1,1 +1,17 @@
+# feladat_009
+"""
+Kérjünk be két egész számot, szám1 és szám2.
+Hasonlítsuk össze a két számot, és irassuk ki hogy
+a szám1 kisebb mint a szám2
+vagy a szám2 kisebb mint a szám1
+vagy a szám1 és a szám2 egyenlő
+"""
+szam1 = input('Egyik szám')
+szam2 = input('másik szám')
+if szam1 < szam2
+print(f"{szam2} a nagyobb")
+if szam1 > szam2
+print(f"{szam1} a nagyobb")
 
+else:
+    print(f"{szam1} és {szam2} egyenlőek ")
