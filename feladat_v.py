59c3052af5beb481f34143d94a9c4fdac25ad664
LakatosJ <128583426+LakatosJ@users.noreply.github.com>
2023-12-07 10:30:46 +0100

    vbn

--- feladat_v.py
+++ feladat_v.py
@@ -1,1 +1,6 @@
+     honapok = ['január', 'február','március', 'április', 'május', 'június']
+
+      for honap in honapok:
+          # az eredeti listaelem NEM módosul!
+	      honap = honap.upper()	
 
